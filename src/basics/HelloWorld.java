3aee4323773fd54d4cf6b029d6780070cab09171
automationtrainingcenter <atcsurya@gmail.com>
2018-11-25 09:47:40 +0530

    modified hello world in github

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,6 +3,7 @@ package basics;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world....");
+		System.out.println("hello from github.....");
 	}
 	
 }
